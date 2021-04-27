76a62ab576d9c5a19725ef6b3a50003df7a622d4
SabrenaMichelle <sabrenaray@yahoo.com>
2021-04-27 19:13:55 -0400

    Functinos Advanced COmpleted

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun (activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+} 
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(expression="*") {
+    return function (noun="special") {
+        return `You are ${expression}${noun}${expression}!`
+    }
+}
+const Calculator={
+   add: function(a,b) {
+       return a + b;
+   },
+   subtract: function (a,b) {
+       return a - b
+   },
+   multiply: function(a,b) {
+       return a * b
+   },
+   divide: function (a,b) {
+       return a / b
+   }
+}
+let actionApplyer = function(start, array){
+    let  base = start
+
+    for (let i=0; i< ray.length; i++){
+        base = ray[i](a)
+    }
+}
